6ea2849f967e3b5a83157debfa7616e839d3a8c6
Julian Sommer <sommerjulian@t-online.de>
2026-04-10 12:00:06 +0200

    finished frontend components + implemented list and bookmarks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,28 @@
 # app/controllers/lists_controller.rb
 class ListsController < ApplicationController
+  def new
+  @list = List.new # <--- Diese Zeile ist entscheidend!
+  end
   def index
     @lists = List.all
   end
 
   def show
   @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list), notice: "List was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
